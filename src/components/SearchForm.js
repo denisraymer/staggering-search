afc47ead86126bffc27cd6bcdbdbc36f8a3692fb
denisraymer <to.mars.66.miles@gmail.com>
2020-12-15 12:34:04 +0300

    Added template

---
+++ src/components/SearchForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Form} from 'react-bootstrap';
+
+function SearchForm() {
+    return (
+        <React.Fragment>
+            <Form.Control type='text' placeholder='Search'/>
+        </React.Fragment>
+    )
+}
+
+export default SearchForm
--- src/page/Main.js
+++ src/page/Main.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import {Col, Container, Row} from 'react-bootstrap';
+import {Col, Container, Row, Table} from 'react-bootstrap';
+import SearchForm from '../components/SearchForm';
 
 function Main() {
     return (
         <Container>
             <Row>
                 <Col xl={12}>
-                    <h1>Main page</h1>
+                    <br/>
+                    <SearchForm/>
+                </Col>
+                <Col xl={12}>
+                    <br/>
+                    <h4>Search results</h4>
+                    <Table striped bordered hover>
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Username</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        </tbody>
+                    </Table>
                 </Col>
             </Row>
         </Container>
