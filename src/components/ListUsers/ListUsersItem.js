39a10ef34c737b99d814fce6ce12858b98a32b66
denisraymer <to.mars.66.miles@gmail.com>
2020-12-18 01:43:07 +0300

    Added modal details component

--- src/components/ListUsers/ListUsersItem.js
+++ src/components/ListUsers/ListUsersItem.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
-import {Button} from 'react-bootstrap';
+import 'rodal/lib/rodal.css';
 import fetchUsers from '../../api/fetchUsers';
+import UserModal from '../UserModal';
 
 function ListUsersItem(props) {
     const {login} = props;
@@ -9,20 +10,18 @@ function ListUsersItem(props) {
 
     useEffect(function () {
         if (response !== null) setUser(response);
+
     }, [response]);
 
     return (
         <tr key={user.node_id}>
-            <td style={{textAlign: 'center'}}>
-                <img src={user.avatar_url} alt={`Avatar ${user.name}`}
-                     style={{borderRadius: '50px', width: '40px'}}/>
+            <td className='text-center'>
+                <img src={user.avatar_url} alt={`Avatar ${user.name}`} className='user-avatar'/>
             </td>
-            <td style={{alignItems: 'middle'}}>{user.name}</td>
-            <td style={{alignItems: 'middle'}}>{user.login}</td>
-            <td style={{alignItems: 'middle'}}>{user.location}</td>
-            <td>
-                <Button variant="light" style={{display: 'block', margin: 'auto'}}>More</Button>
-            </td>
+            <td>{user.name}</td>
+            <td>{user.login}</td>
+            <td>{user.location}</td>
+            <td><UserModal user={user}/></td>
         </tr>
     )
 }
